87b6ae1f7cfbd151704dcd43d93bac6d434276cb
Lazar <dima.lazarev@cheitgroup.com>
2024-03-25 20:20:11 +0200

    add all static code

--- assets/main.js
+++ assets/main.js
@@ -7,7 +7,37 @@ const pageLoad = (cb) => {
     window.addEventListener('load', cb);
 };
 
+const fadeAnimation = () => {
+    const SELECTORS = {
+        el: '.js-fade-el',
+    };
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    const $fadeEl = document.querySelectorAll(SELECTORS.el);
+    if ($fadeEl.length) {
+        gsap.set($fadeEl, {
+            y: 20,
+            opacity: 0,
+        });
+
+        ScrollTrigger.batch($fadeEl, {
+            start: 'top 85%',
+            once: true,
+            onEnter: (batch) =>
+                gsap.to(batch, {
+                    duration: 0.4,
+                    opacity: 1,
+                    y: 0,
+                    stagger: 0.1,
+                    ease: 'none',
+                }),
+        });
+    }
+};
+
 pageLoad(() => {
     document.body.classList.add('body--loaded');
+    fadeAnimation();
 });
 
